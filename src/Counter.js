840655e02d097e0eafeb13d9e7118f9062a1bc17
Arash dehghani <74924922+arashdehghani1400@users.noreply.github.com>
2023-12-13 18:42:24 +0000

    commit final code

--- src/Counter.js
+++ src/Counter.js
@@ -4,11 +4,11 @@ const Counter = () => {
     const [count, setCount] = useState(0);
 return (
 // Component JSX goes here
-<div style={{ textAlign: 'center', marginTop: '50px' }}>
-<h1 style={{ fontSize: '2em' }}>Count: {count}</h1>
-<button onClick={() => setCount(count + 1)}>Increment</button>
-<button onClick={() => setCount(count > 0 ? count - 1 : 0)}>Decrement</button>
-</div>
+  <div style={{ textAlign: 'center', marginTop: '50px' }}>
+  <h1 style={{ fontSize: '2em' }}>Count: {count}</h1>
+  <button onClick={() => setCount(count + 1)}>Increment</button>
+  <button onClick={() => setCount(count > 0 ? count - 1 : 0)}>Decrement</button>
+  </div>
 );
 };
 export default Counter;
